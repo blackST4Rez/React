9505353c698c7c11f972085f36bc001bfcac6ac3
blackST4Rez <maharjanraka2015@gmail.com>
2026-08-04 02:36:14 +0545

    React hooks - useState.

---
+++ Basics/07-useState/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+
+const App = () => {
+
+  const [num, setNum] = useState(10)
+
+  function increaseNum() {
+    setNum(num+1)
+  }
+  function decreaseNum() {
+    setNum(num-1)
+  }
+  function jump5Num() {
+    setNum(num+5)
+  }
+
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>increase</button>
+      <button onClick={decreaseNum}>decrease</button>
+      <button onClick={jump5Num}>Increas by 5</button>
+    </div>
+  )
+}
+
+export default App
