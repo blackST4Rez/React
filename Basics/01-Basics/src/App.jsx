e36be2a30aa5efa0b598487c76c912e40965b2c9
blackST4Rez <maharjanraka2015@gmail.com>
2026-08-03 12:18:44 +0545

    Basics of React

---
+++ Basics/01-Basics/src/App.jsx
@@ -0,0 +1,24 @@
+const App = () => {
+  return (
+    <>
+      <div id="parent">
+        <h1 id="child1">Hello World</h1>
+        <h2 id="child2">Hello World</h2>
+        <h3 id="child3">Hello World</h3>
+        <h4 id="child4">Hello World</h4>
+        <h5 id="child5">Hello World</h5>
+        <h6 id="child6">Hello World</h6>
+      </div>
+      <div id="sibiling">
+        <h6>Hello World</h6>
+        <h5>Hello World</h5>
+        <h4>Hello World</h4>
+        <h3>Hello World</h3>
+        <h2>Hello World</h2>
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
