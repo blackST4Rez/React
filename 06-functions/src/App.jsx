5879f2645d39a8542b21343b88c220b4d1b488f0
blackST4Rez <maharjanraka2015@gmail.com>
2026-08-11 18:21:32 +0545

    types of functions revised.

--- 06-functions/src/App.jsx
+++ 06-functions/src/App.jsx
@@ -1,13 +1,41 @@
 
 const App = () => {
-  function btnClicked(){
-    console.log('hello');
-  }
-  return (
-    <div>
-      <button onClick={btnClicked}>Click</button>
-    </div>
-  )
+
+  /* Named Function 
+    function handleClick() {
+        console.log('Button was clicked!');
+        alert('Hello World!');
+    } */
+  
+  /* Named Inline Function
+    function handleClick(message) {
+        console.log(message);
+        alert(message);
+    } */
+  
+  /* Arrow Function
+      /* const handleClick = () => {
+      console.log('Button was clicked!');
+    }; */
+  
+  /* Inline Arrow Function
+  const handleClick = (message) => {
+        console.log(message);
+    }; */
+    
+    return (
+      {/* <button onClick={handleClick('Raka Maharjan')}>
+            Click Me
+      </button> */}
+
+      /* For Inline Arrow Function
+      <button onClick={() => handleClick('Raka Maharjan')}>
+            Click Me
+      </button> */
+      
+    )
 }
 
 export default App
+
+/* We can write  () => before function with paramete if we dont wanna run it immediately */
