69dfa18badc56b957bb4681fa2f7cf5eaad4c1d0
blackST4Rez <maharjanraka2015@gmail.com>
2026-08-11 18:04:08 +0545

    Prop drilling revised.

--- 04-CardswithPropsDrilling/src/App.jsx
+++ 04-CardswithPropsDrilling/src/App.jsx
@@ -108,11 +108,11 @@ const App = () => {
 
   return (
     <div className='parent'>
-      {jobOpenings.map(function (elem, idx) {
-        return <div key={idx}>
+      {jobOpenings.map((elem, idx) => (
+        <div key={idx}>
           <Card company={elem.companyName} datePosted={elem.datePosted} post={elem.post} tag1={elem.tag1} brandLogo={elem.brandLogo} pay={elem.pay} tag2={elem.tag2} />
         </div>
-      })}
+      ))}
     </div>
   )
 }
